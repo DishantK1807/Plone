fdfbaaab3ca9d051b4f54be03d04920cadf31cdc
Hanno Schlichting <hanno@hannosch.eu>
2008-11-29 17:17:41 +0000

    Merged the selectedTabs script into the `portal_tabs_view`.
    svn path=/Plone/trunk/; revision=23479

--- Products/CMFPlone/browser/navigation.py
+++ Products/CMFPlone/browser/navigation.py
@@ -186,6 +186,37 @@ class CatalogNavigationTabs(BrowserView):
                 result.append(data)
         return result
 
+    def selectedTabs(self, default_tab='index_html', tabs=[]):
+        context = aq_inner(self.context)
+
+        url_tool = getToolByName(context, 'portal_url')
+        plone_url = url_tool()
+        request = self.request
+        valid_actions = []
+
+        url = request['URL']
+        path = url[len(plone_url):]
+
+        for action in tabs:
+            if not action['url'].startswith(plone_url):
+                # In this case the action url is an external link. Then, we avoid 
+                # issues (bad portal_tab selection) continuing with next action.
+                continue
+            action_path = action['url'][len(plone_url):]
+            if not action_path.startswith('/'):
+                action_path = '/' + action_path
+            if path.startswith(action_path):
+                # Make a list of the action ids, along with the path length for
+                # choosing the longest (most relevant) path.
+                valid_actions.append((len(action_path), action['id']))
+
+        # Sort by path length, the longest matching path wins
+        valid_actions.sort()
+        if valid_actions:
+            return {'portal':valid_actions[-1][1]}
+
+        return {'portal':default_tab}
+
 
 class CatalogNavigationBreadcrumbs(BrowserView):
     implements(INavigationBreadcrumbs)
