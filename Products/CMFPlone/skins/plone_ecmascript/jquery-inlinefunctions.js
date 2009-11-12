b06fce5395f95fd9b877f394289e90b3f0bfed92
Eric Steele <esteele@plone.org>
2009-11-12 04:28:51 +0000

    An initial pass at implementing inline form validation using jQuery. Uses the KSS html structure and backend views, but with a (hopefully) lighter layer in between.
    svn path=/Plone/branches/esteele-jqueryrewrite/; revision=31321

---
+++ Products/CMFPlone/skins/plone_ecmascript/jquery-inlinefunctions.js
@@ -0,0 +1,71 @@
+
+function getKSSAttr(obj, varname){
+    classes = obj.attr('class').split(' ');
+    classname = 'kssattr-' + varname + '-';
+    for (i in classes){
+        cls = classes[i];
+        if(cls.indexOf(classname) >= 0){
+            return cls.substring(classname.length, cls.length);
+        }
+    }
+    return '';
+}
+
+/* Inline Validation */
+jQuery(function(){
+    jQuery('input.blurrable, select.blurrable, textarea.blurrable').blur(function(){
+        value = jQuery(this).val();
+        wrapperdiv = jQuery(this).parents("div[class*='kssattr-atfieldname']");
+        fieldname = getKSSAttr(wrapperdiv, 'atfieldname');
+        jQuery.get(document.baseURI + '/' + '@@kssValidateField', {'fieldname': fieldname, 'value':value},
+          function(data){
+              clearCommand = jQuery(data).find('command[name="clearChildNodes"]');
+              if (clearCommand.length > 0){
+                  selector = jQuery(clearCommand).attr('selector');
+                  jQuery(selector).empty();
+              }
+              setAttributeCommand = jQuery(data).find('command[name="setAttribute"]');
+              if (setAttributeCommand.length > 0){
+                  selector = jQuery(setAttributeCommand).attr('selector');
+                  attributeName = jQuery(setAttributeCommand).find('param[name="name"]').text();
+                  replaceText = jQuery(setAttributeCommand).find('param[name="value"]').text();
+                  jQuery('#' + selector).attr(attributeName, replaceText);
+              }
+              replaceInnerHTMLCommand = jQuery(data).find('command[name="replaceInnerHTML"]');
+              if (replaceInnerHTMLCommand.length > 0){
+                  selector = jQuery(replaceInnerHTMLCommand).attr('selector');
+                  replaceHTML = jQuery(replaceInnerHTMLCommand).find('param[name="html"]').text();
+                  jQuery(selector).html(replaceHTML);
+              }
+          });
+    });
+
+});
+
+
+
+// Possible responses:
+// 
+// <!-- xmlns="http://www.kukit.org/commands/1.1" removed from kukit tag as it
+//      breaks IE6 XP SP3 -->
+// <commands>
+// <command selector="div#archetypes-fieldname-title div.fieldErrorBox" name="clearChildNodes" selectorType="css">
+// </command>
+// <command selector="archetypes-fieldname-title" name="setAttribute" selectorType="htmlid">
+//     <param name="name">class</param>
+//     <param name="value">field Archetypestitlefield  kssattr-atfieldname-title</param>
+// </command>
+// </commands
+
+
+// <commands>
+// <command selector="div#archetypes-fieldname-title div.fieldErrorBox" name="replaceInnerHTML" selectorType="css">
+//     <param name="html"><![CDATA[Title is required, please correct.]]></param>
+//     <param name="withKssSetup">True</param>
+// </command>
+// <command selector="archetypes-fieldname-title" name="setAttribute" selectorType="htmlid">
+//     <param name="name">class</param>
+//     <param name="value">field error Archetypestitlefield  kssattr-atfieldname-title</param>
+// </command>
+// </commands>
+// </kukit>
