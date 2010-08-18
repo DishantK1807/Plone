bd191eb57d210510af3c76d3b1d5735f055d09f9
Laurence Rowe <l@lrowe.co.uk>
2010-08-18 16:38:04 +0000

    Products.CMFPlone
    svn path=/Plone/branches/plip10877-products-cmfplone/; revision=38809

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ import os.path
 
 version = '4.0rc2'
 
-setup(name='Plone',
+setup(name='Products.CMFPlone',
       version=version,
-      description="The Plone Content Management System",
+      description="The Plone Content Management System (core)",
       long_description=open("README.txt").read() + "\n" +
                        open(os.path.join("docs", "CHANGES.txt")).read(),
       classifiers=[
