fa46d57cc0eda1eb55e20ff77fc9abdcba52065b
Denys Mishunov <denys.mishunov@gmail.com>
2010-09-24 09:30:36 +0000

    wrapped content of search.pt into proper JQuery wrapper so that the code would be loaded after DOM
    svn path=/Plone/branches/plip9352-search-ui/; revision=40194

--- Products/CMFPlone/skins/plone_ecmascript/search.js
+++ Products/CMFPlone/skins/plone_ecmascript/search.js
@@ -1,97 +1,101 @@
-var container = jq('#search-results');
+(function($) { $(function() {
+    var container = $('#search-results');
 
-jq("form.searchPage").submit(function() {
-    var data = jq("form.searchPage").serialize();
-    jq(container).fadeOut('fast');
-    updateResults(data);
-    return false;
-}); 
-jq("#search-filter input").bind('change', function() {
-    var data = jq("form.searchPage").serialize();
-    jq(container).fadeOut('fast');
-    updateResults(data);
-    return false;
-});
-
-function submitForm() {
-    var data = jq("form.searchPage").serialize();
-    jq(container).fadeOut('fast');
-    updateResults(data);
-    return false;                        
-}
-
-jq('#searchResultsHeading').click(function(event) {
-   //jq("#search-results").fadeOut(300);
-   if (jq(event.target).is('#sorting-options a')) {
-       if (jq(event.target).attr('rel')) {
-           rel = jq(event.target).attr('rel');                               
-           jq("form.searchPage input[name='sort_on']").val(rel);                               
-       }
-       else {
-           jq("form.searchPage input[name='sort_on']").val("");
-       }
-       var data = jq("form.searchPage").serialize();   
-       updateResults(data);
-       // jq("#search-results").before(jq("#kssPortalMessage"));
-       // jq("#kssPortalMessage dd").html("Your search results have been updated");
-       // 
-       // jq("#kssPortalMessage").delay(500).animate({
-       //     height: ['toggle', 'swing'],
-       //     opacity: 1,
-       //     marginBottom: 'toggle',
-       //     marginTop: 'toggle'
-       // }, 500, function(){
-       //     jq("*").click(function(){
-       //         jq("#kssPortalMessage").fadeOut('slow');
-       //     })
-       // })
-       return false;
-   }
- });
-
-
-function updateResults(data){
-    jq.ajax({
-        url: '@@updated_search',
-        data: data,
-        success: function(data){
-            container.hide();
-            container.html(data);
-            jq(container).fadeIn('medium');
-            jq("#search-results-number").text(function(){
-                str = jq("#updated-search-results-number").text();
-                jq("#updated-search-results-number").remove();
-                return str;
-            });
-            jq("#searchResultsHeading #sorting-options").html(function(){
-                struct = jq("#updated-sorting-options").html();
-                jq("#updated-sorting-options").remove();
-                return struct;
-            });  
-        },
-        error: function(req,error){
-            if(error === 'error'){error = req.statusText;}
-            var errormsg = 'There was a communication error: ' + error;
-            container.html(errormsg);
-        }
+    $("form.searchPage").submit(function() {
+        var data = $("form.searchPage").serialize();
+        $(container).fadeOut('fast');
+        updateResults(data);
+        return false;
+    }); 
+    $("#search-filter input").bind('change', function() {
+        var data = $("form.searchPage").serialize();
+        $(container).fadeOut('fast');
+        updateResults(data);
+        return false;
     });
-}                    
 
-jq('#show-search-options').click(function () {                        
-    jq("#search-results-wrapper").css({"width":"97.75%", "margin-left":"-98.875%"});
-    jq("#search-results-wrapper").removeClass("width-16");
-    jq("#search-results-wrapper").removeClass("position-0");
-    jq('#search-filter').hide();
-    jq('#search-filter').removeClass('hiddenStructure');
+    function submitForm() {
+        var data = $("form.searchPage").serialize();
+        $(container).fadeOut('fast');
+        updateResults(data);
+        return false;                        
+    }
+
+    $('#searchResultsHeading').click(function(event) {
+       //$("#search-results").fadeOut(300);
+       if ($(event.target).is('#sorting-options a')) {
+           if ($(event.target).attr('rel')) {
+               rel = $(event.target).attr('rel');                               
+               $("form.searchPage input[name='sort_on']").val(rel);                               
+           }
+           else {
+               $("form.searchPage input[name='sort_on']").val("");
+           }
+           var data = $("form.searchPage").serialize();   
+           updateResults(data);
+           // $("#search-results").before($("#kssPortalMessage"));
+           // $("#kssPortalMessage dd").html("Your search results have been updated");
+           // 
+           // $("#kssPortalMessage").delay(500).animate({
+           //     height: ['toggle', 'swing'],
+           //     opacity: 1,
+           //     marginBottom: 'toggle',
+           //     marginTop: 'toggle'
+           // }, 500, function(){
+           //     $("*").click(function(){
+           //         $("#kssPortalMessage").fadeOut('slow');
+           //     })
+           // })
+           return false;
+       }
+     });
+
+
+    function updateResults(data){
+        $.ajax({
+            url: '@@updated_search',
+            data: data,
+            success: function(data){
+                container.hide();
+                container.html(data);
+                $(container).fadeIn('medium');
+                $("#search-results-number").text(function(){
+                    str = $("#updated-search-results-number").text();
+                    $("#updated-search-results-number").remove();
+                    return str;
+                });
+                $("#searchResultsHeading #sorting-options").html(function(){
+                    struct = $("#updated-sorting-options").html();
+                    $("#updated-sorting-options").remove();
+                    return struct;
+                });  
+            },
+            error: function(req,error){
+                if(error === 'error'){error = req.statusText;}
+                var errormsg = 'There was a communication error: ' + error;
+                container.html(errormsg);
+            }
+        });
+    }                    
+
+    $('#show-search-options').click(function () {                        
+        $("#search-results-wrapper").css({"width":"97.75%", "margin-left":"-98.875%"});
+        $("#search-results-wrapper").removeClass("width-16");
+        $("#search-results-wrapper").removeClass("position-0");
+        $('#search-filter').hide();
+        $('#search-filter').removeClass('hiddenStructure');
+
+        $("#search-results-wrapper").animate({
+            width: '72.75%',
+            marginLeft: '-73.875%'
+        }, 500, function(){
+            $('#search-filter').fadeIn('medium');
+            $("#show-search-options").delay(500).fadeOut();
+        });
+        $("#search-results-wrapper").addClass("position-1:4");
+        $("#search-results-wrapper").addClass("width-12");                        
+        return false;
+    });
     
-    jq("#search-results-wrapper").animate({
-        width: '72.75%',
-        marginLeft: '-73.875%'
-    }, 500, function(){
-        jq('#search-filter').fadeIn('medium');
-        jq("#show-search-options").delay(500).fadeOut();
-    });
-    jq("#search-results-wrapper").addClass("position-1:4");
-    jq("#search-results-wrapper").addClass("width-12");                        
-    return false;
-});
+}); })(jQuery);
+
