090a204ef98db54c570eea21fd67d81a92fe2777
Elizabeth Leddy <elizabeth.leddy@gmail.com>
2010-10-31 16:08:57 +0000

    convenience functions for calvins mom
    svn path=/Plone/branches/selenium-integration/; revision=44761

--- Products/CMFPlone/selenium/base.py
+++ Products/CMFPlone/selenium/base.py
@@ -25,4 +25,42 @@ class SeleniumTestCase(unittest2.TestCase):
         self.driver.find_element_by_name('__ac_name').send_keys(username)
         self.driver.find_element_by_name('__ac_password').send_keys(password)
         self.driver.find_element_by_name('submit').click()
+        
+        
+    
+    '''
+    Convenience functions
+    If you record the selenium tests in Firefox IDE and then export as web driver
+    junit format, you should be able to replace "selenium." with "self.". It's 
+    not a catch all but should help a bit.
+    '''
+    def click(self, xpath):
+        if xpath.count("link="):
+            link = xpath.split("link=")[-1]
+            element = self.driver.find_element_by_partial_link_text(link)
+        elif xpath.count("//"):
+            element = self.driver.find_element_by_xpath(xpath)
+        elif xpath.count('#'):
+            eleName = xpath.split("#")[-1]
+            element = self.driver.find_element_by_id(eleName)
+        else:
+            element = self.driver.find_element_by_name(xpath)
+        
+        element.click()
+        
+    def type(self, name, value):
+        self.driver.find_element_by_name(name).send_keys(value)
+        
+    def select(self, xpath1, xpath2=''):
+        xpath = xpath1
+        if xpath2:
+            xpath = "%s[%s]"%(xpath1, xpath2)
+        self.driver.find_element_by_xpath(xpath).set_selected()
+        
+    def waitForPageToLoad(self, foo):
+        pass
+        
+    def publish(self):
+        self.click("//dl[@id='plone-contentmenu-workflow']/dt/a")
+        self.click("#workflow-transition-publish") 
 
---
+++ Products/CMFPlone/selenium/testPortlets.py
@@ -0,0 +1,31 @@
+from Products.CMFPlone.selenium.base import SeleniumTestCase
+
+class TestPortlets(SeleniumTestCase):    
+    
+    def test_add_collection(self):
+        self.portal.acl_users._doAddUser('manager', 'secret', ['Manager'], [])
+        self.login('manager', 'secret')
+        self.open('/')
+        self.click("//dl[@id='plone-contentmenu-factories']/dt/a")
+        self.click("#folder")
+        self.type("title", "test")
+        self.click("form.button.save")
+        #self.publish()
+        self.click("link=Manage portlets")
+        self.select("//div[@id='portletmanager-plone-rightcolumn']/div[1]/form/select", "label=Static text portlet")
+        self.type("form.header", "Better Header")
+        self.type("form.footer", "I'll eat pancakes on your grave")
+        self.click("link=Home")
+        self.click("form.actions.save")
+        self.click("link=test")
+        self.click("link=Manage portlets")
+        self.select("//div[@id='portletmanager-plone-rightcolumn']/div[3]/form/div[1]/select", "label=Block")
+        self.click("//div[@id='portletmanager-plone-rightcolumn']/div[3]/form/div[4]/input")
+        self.click("link=test")
+        self.click("//span[@id='breadcrumbs-home']/a")
+        self.click("link=Manage portlets")
+        self.click("link=Better Header")
+        self.click("form.omit_border")
+        self.click("form.actions.save")
+        self.click("link=Home")
+        
