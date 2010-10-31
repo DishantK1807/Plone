5eee2c98a5621fcf7073d5de5dedc09566c0a6b0
Eric Steele <esteele@plone.org>
2010-10-31 12:58:16 +0000

    Select all options, save.
    svn path=/Plone/branches/selenium-integration/; revision=44745

--- Products/CMFPlone/selenium/testCollections.py
+++ Products/CMFPlone/selenium/testCollections.py
@@ -6,22 +6,38 @@ class TestCollections(SeleniumTestCase):
         self.portal.acl_users._doAddUser('manager', 'secret', ['Manager'], [])
         self.login('manager', 'secret')
         self.open('/')
+        
+        # Add a new collection
         self.driver.find_element_by_partial_link_text('Add new').click()
         self.driver.find_element_by_partial_link_text('Collection').click()
+        
+        # Define title, description
         self.driver.find_element_by_name('title').send_keys('I am a collection with javascript')
         self.driver.find_element_by_name('description').send_keys('skkep skip skop')
-        self.driver.find_element_by_xpath("id('customViewFields_options')/option[attribute::value='CreationDate']").set_selected()
-        self.driver.find_element_by_xpath("id('customViewFields_options')/option[attribute::value='Creator']").set_selected()
-        self.driver.find_element_by_xpath("id('customViewFields_options')/option[attribute::value='Description']").set_selected()
-        self.driver.find_element_by_xpath("id('customViewFields_options')/option[attribute::value='EffectiveDate']").set_selected()
-        self.driver.find_element_by_xpath("id('customViewFields_options')/option[attribute::value='end']").set_selected()
-        self.driver.find_element_by_xpath("id('customViewFields_options')/option[attribute::value='ExpirationDate']").set_selected()
-        self.driver.find_element_by_xpath("id('customViewFields_options')/option[attribute::value='getId']").set_selected()
-        self.driver.find_element_by_xpath("id('customViewFields_options')/option[attribute::value='getObjSize']").set_selected()
-        self.driver.find_element_by_xpath("id('customViewFields_options')/option[attribute::value='location']").set_selected()
-        self.driver.find_element_by_xpath("id('customViewFields_options')/option[attribute::value='ModificationDate']").set_selected()
-        self.driver.find_element_by_xpath("id('customViewFields_options')/option[attribute::value='review_state']").set_selected()
-        self.driver.find_element_by_xpath("id('customViewFields_options')/option[attribute::value='start']").set_selected()
-        self.driver.find_element_by_xpath("id('customViewFields_options')/option[attribute::value='Subject']").set_selected()
-        self.driver.find_element_by_xpath("id('customViewFields_options')/option[attribute::value='Type']").set_selected()
         
+        # Display as Table
+        self.driver.find_element_by_id('customView').click()
+        
+        # Select all available table columns for viewing    
+        options = ['CreationDate',
+                   'Creator',
+                   'Description', 
+                   'EffectiveDate',
+                   'end', 
+                   'ExpirationDate', 
+                   'getId', 
+                   'getObjSize', 
+                   'location',
+                   'ModificationDate', 
+                   'review_state', 
+                   'start', 
+                   'Subject',
+                   'Type']
+        
+        for opt in options:
+            self.driver.find_element_by_xpath("id('customViewFields_options')/option[attribute::value='%s']" % opt).set_selected()
+
+        self.driver.find_element_by_xpath("//input[attribute::value='>>']").click()
+        
+        # Save the collection
+        self.driver.find_element_by_name('form.button.save').click()
