1bde1da30d776561adf726bc39a82964c816448c
Eric Steele <esteele@plone.org>
2010-10-31 10:29:31 +0000

    Refactor base testcase.
    svn path=/Plone/branches/selenium-integration/; revision=44735

---
+++ Products/CMFPlone/selenium/base.py
@@ -0,0 +1,27 @@
+import transaction
+import unittest2
+
+from plone.app.testing import TEST_USER_NAME
+from plone.app.testing import TEST_USER_PASSWORD
+from plone.app.testing.selenium_layers import SELENIUM_TESTING
+from plone.app.testing import selenium_layers as layers
+
+
+class SeleniumTestCase(unittest2.TestCase):
+    layer = SELENIUM_TESTING
+
+    def setUp(self):
+        self.driver = self.layer['selenium']
+    
+    def open(self, path="/"):
+        # ensure we have a clean starting point
+        transaction.commit()
+        portal = self.layer['portal']
+        self.driver.get("%s%s" % (portal.absolute_url(), path))
+
+    def login(self, username=TEST_USER_NAME, password=TEST_USER_PASSWORD):
+        self.open('/login_form')
+        self.driver.find_element_by_name('__ac_name').send_keys(username)
+        self.driver.find_element_by_name('__ac_password').send_keys(password)
+        self.driver.find_element_by_name('submit').click()
+
---
+++ Products/CMFPlone/selenium/testCollections.py
@@ -0,0 +1,8 @@
+from Products.CMFPlone.selenium.base import SeleniumTestCase
+
+class TestCollections(SeleniumTestCase):
+
+    def test_add_collection(self):
+        self.login()
+        
+    
