74ca49350a66cf5e704f797c97e6c6ac2be9b792
Denys Mishunov <denys.mishunov@gmail.com>
2010-11-05 20:35:59 +0000

    Merged changes from trunk to the corresponding branches for PLIP 9352
    svn path=/Plone/branches/plip9352-search-ui/; revision=45496

--- Products/CMFPlone/skins/plone_ecmascript/search.js
+++ Products/CMFPlone/skins/plone_ecmascript/search.js
@@ -1,24 +1,24 @@
-(function($) { $(function() {
+(function ($) { $(function () {
     var container = $('#search-results');
 
-    $("form.searchPage").submit(function() {
+    $("form.searchPage").submit(function () {
         var data = $("form.searchPage").serialize();
         $(container).fadeOut('fast');
         updateResults(data);
         return false;
     }); 
-    $("#search-filter input, #search-filter select").bind('change', function() {
+    $("#search-filter input, #search-filter select").bind('change', function () {
         var data = $("form.searchPage").serialize();
         $(container).fadeOut('fast');
         updateResults(data);
         return false;
     });
 
-    $('#searchResultsHeading').click(function(event) {
+    $('#searchResultsHeading').click(function (event) {
        //$("#search-results").fadeOut(300);
        if ($(event.target).is('#sorting-options a')) {
            if ($(event.target).attr('rel')) {
-               rel = $(event.target).attr('rel');                               
+               var rel = $(event.target).attr('rel');
                $("form.searchPage input[name='sort_on']").val(rel);                               
            }
            else {           
@@ -34,8 +34,8 @@
            //     opacity: 1,
            //     marginBottom: 'toggle',
            //     marginTop: 'toggle'
-           // }, 500, function(){
-           //     $("*").click(function(){
+           // }, 500, function () {
+           //     $("*").click(function () {
            //         $("#kssPortalMessage").fadeOut('slow');
            //     })
            // })
@@ -44,28 +44,28 @@
      });
 
 
-    function updateResults(data){
-        console.log(data);
+    function updateResults(data) {
+
         $.ajax({
             url: '@@updated_search',
             data: data,
-            success: function(data){
+            success: function (data) {
                 container.hide();
                 container.html(data);
                 $(container).fadeIn('medium');
-                $("#search-results-number").text(function(){
-                    str = $("#updated-search-results-number").text();
+                $("#search-results-number").text(function () {
+                    var str = $("#updated-search-results-number").text();
                     $("#updated-search-results-number").remove();
                     return str;
                 });
-                $("#searchResultsHeading #sorting-options").html(function(){
+                $("#searchResultsHeading #sorting-options").html(function () {
                     struct = $("#updated-sorting-options").html();
                     $("#updated-sorting-options").remove();
                     return struct;
                 });  
             },
-            error: function(req,error){
-                if(error === 'error'){error = req.statusText;}
+            error: function (req,error) {
+                if(error === 'error') {error = req.statusText;}
                 var errormsg = 'There was a communication error: ' + error;
                 container.html(errormsg);
             }
@@ -82,7 +82,7 @@
         $("#search-results-wrapper").animate({
             width: '72.75%',
             marginLeft: '-73.875%'
-        }, 500, function(){
+        }, 500, function () {
             $('#search-filter').fadeIn('medium');
             $("#show-search-options").delay(500).fadeOut();
         });
@@ -94,12 +94,12 @@
     $("#close-search-options").click(function () {
         $("#search-results-wrapper").removeClass("width-12");
         $("#search-results-wrapper").removeClass("position-1:4");
-        $('#search-filter').fadeOut('fast', function(){
+        $('#search-filter').fadeOut('fast', function () {
             $('#search-filter').addClass('hiddenStructure');            
             $("#search-results-wrapper").animate({
                 width: '97.75%',
                 marginLeft: '-98.875%'
-            }, 500, function(){
+            }, 500, function () {
                 $("#show-search-options").delay(300).fadeIn();
             });            
         });
