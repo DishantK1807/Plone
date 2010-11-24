1597b1dfc78b954da1850f2edb0a9e12923db35b
Denys Mishunov <denys.mishunov@gmail.com>
2010-11-24 20:28:01 +0000

    code cleaning in compliance with JSLint
    svn path=/Plone/branches/plip9352-search-ui/; revision=45944

--- Products/CMFPlone/skins/plone_ecmascript/search.js
+++ Products/CMFPlone/skins/plone_ecmascript/search.js
@@ -1,3 +1,6 @@
+/* The following line defines gliobal variables defined elsewhere. */
+/*global jQuery:false*/
+
 (function ($) {
 
     $(function () {
@@ -14,16 +17,18 @@
                     container.hide();
                     container.html(data);
                     $(container).fadeIn('medium');
-                    $("#search-results-number").text(function () {
-                        str = $("#updated-search-results-number").text();
-                        $("#updated-search-results-number").remove();
+                    $('#search-results-number').text(function () {
+                        str = $('#updated-search-results-number').text();
+                        $('#updated-search-results-number').remove();
                         return str;
                     });
-                    $("#searchResultsHeading #sorting-options").html(function () {
-                        struct = $("#updated-sorting-options").html();
-                        $("#updated-sorting-options").remove();
-                        return struct;
-                    });
+                    $('#searchResultsHeading #sorting-options').html(
+                        function () {
+                            struct = $('#updated-sorting-options').html();
+                            $('#updated-sorting-options').remove();
+                            return struct;
+                        }
+                    );
                 },
                 error: function (req, error) {
                     return true;
@@ -31,19 +36,21 @@
             });
         }
 
-        $("form.searchPage").submit(function () {
-            data = $("form.searchPage").serialize();
+        $('form.searchPage').submit(function () {
+            data = $('form.searchPage').serialize();
             $(container).fadeOut('fast');
             updateResults(data);
             return false;
         });
 
-        $("#search-filter input, #search-filter select").bind('change', function () {
-            data = $("form.searchPage").serialize();
-            $(container).fadeOut('fast');
-            updateResults(data);
-            return false;
-        });
+        $('#search-filter input, #search-filter select').bind('change', 
+            function () {
+                data = $('form.searchPage').serialize();
+                $(container).fadeOut('fast');
+                updateResults(data);
+                return false;
+            }
+        );
 
         $('#sorting-options a').live('click', function () {
             if ($(this).attr('rel')) {
@@ -52,45 +59,46 @@
             else {
                 $("form.searchPage input[name='sort_on']").val('');
             }
-            data = $("form.searchPage").serialize();
+            data = $('form.searchPage').serialize();
             updateResults(data);
             return false;
         });
 
         $('#show-search-options').click(function () {
-            $("#search-results-wrapper").css({"width": "97.75%", "margin-left": "-98.875%"});
-            $("#search-results-wrapper").removeClass("width-16");
-            $("#search-results-wrapper").removeClass("position-0");
+            $('#search-results-wrapper').css({'width': '97.75%', 
+                                              'margin-left': '-98.875%'});
+            $('#search-results-wrapper').removeClass('width-16');
+            $('#search-results-wrapper').removeClass('position-0');
             $('#search-filter').hide();
             $('#search-filter').removeClass('hiddenStructure');
 
-            $("#search-results-wrapper").animate({
+            $('#search-results-wrapper').animate({
                 width: '72.75%',
                 marginLeft: '-73.875%'
             }, 500, function () {
                 $('#search-filter').fadeIn('medium');
-                $("#show-search-options").delay(500).fadeOut();
+                $('#show-search-options').delay(500).fadeOut();
             });
-            $("#search-results-wrapper").addClass("position-1:4");
-            $("#search-results-wrapper").addClass("width-12");
+            $('#search-results-wrapper').addClass('position-1:4');
+            $('#search-results-wrapper').addClass('width-12');
             return false;
         });
 
-        $("#close-search-options").click(function () {
-            $("#search-results-wrapper").removeClass("width-12");
-            $("#search-results-wrapper").removeClass("position-1:4");
+        $('#close-search-options').click(function () {
+            $('#search-results-wrapper').removeClass('width-12');
+            $('#search-results-wrapper').removeClass('position-1:4');
             $('#search-filter').fadeOut('fast', function () {
                 $('#search-filter').addClass('hiddenStructure');
-                $("#search-results-wrapper").animate({
+                $('#search-results-wrapper').animate({
                     width: '97.75%',
                     marginLeft: '-98.875%'
                 }, 500, function () {
-                    $("#show-search-options").delay(300).fadeIn();
+                    $('#show-search-options').delay(300).fadeIn();
                 });
             });
-            $("#search-results-wrapper").addClass("position-0");
-            $("#search-results-wrapper").addClass("width-16");
+            $('#search-results-wrapper').addClass('position-0');
+            $('#search-results-wrapper').addClass('width-16');
             return false;
         });
     });
-})(jQuery);
+}(jQuery));
